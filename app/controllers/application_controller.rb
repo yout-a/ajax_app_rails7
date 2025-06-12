d2a63a8b05a5c12fe3360f44551b6b7904b9f9ee
yout-a <rainyday1388@gmail.com>
2025-06-13 00:00:40 +0900

    Update application_controller.rb

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,11 +1,12 @@
 class ApplicationController < ActionController::Base
-   before_action :basic_auth
+  before_action :basic_auth
 
-   private
+  private
 
   def basic_auth
     authenticate_or_request_with_http_basic do |username, password|
-      username == ENV["BASIC_AUTH_USER"] && password == ENV["BASIC_AUTH_PASSWORD"]
+      puts "認証チェック: #{username} / #{password}" # ←ログ出力確認
+      username == "admin" && password == "2222"
     end
   end
 end
